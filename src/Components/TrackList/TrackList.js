40740fd867d38f19550a730ab79cc1e075ffad26
Create <createinnovateproduce@gmail.com>
2018-02-10 22:41:27 -0600

    Initial commit

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+let trackArray;
+
+export class TrackList extends React.Component {
+  mapTracks() {
+    trackArray = Array.from(this.props.tracks);
+    console.log("TrackList trackArray: " + trackArray);
+    return (trackArray.map(track => {
+      return (
+        <Track
+          track={track}
+          key={track.id}
+          onAdd={this.props.onAdd}
+          onRemove={this.props.onRemove}
+          isRemoval={this.props.isRemoval}
+        />
+      );
+    }));
+  };
+  render() {
+    return (
+      <div className="TrackList">
+        { this.mapTracks() }
+      </div>
+    );
+  }
+};
+
+export default TrackList;
---
+++ src/util/Spotify.js
@@ -0,0 +1,113 @@
+let accessToken;
+const cliendId = '604295493f1c4fa4a3af6cbaa281a07a';
+const redirectUri = 'https://jamming-mihailo.surge.sh/';
+const apiUrl = 'https://api.spotify.com/v1/';
+const crossUrl = 'https://cors-anywhere.herokuapp.com/';
+
+let Spotify = {
+  getAccessToken: function(){
+    if(accessToken){
+      return accessToken;
+    } else if (window.location.href.match(/access_token=([^&]*)/)){
+      accessToken = (window.location.href.match(/access_token=([^&]*)/))[1];
+      let expiresIn = (window.location.href.match(/expires_in=([^&]*)/))[1];
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      let scopes = "playlist-modify-private playlist-modify-public";
+      let url = `https://accounts.spotify.com/authorize?client_id=${cliendId}&response_type=token&scope=${encodeURIComponent(scopes)}&redirect_uri=${redirectUri}`;
+      window.location.assign(url);
+    }
+  },
+  search: function(term){
+    let searchUrl = `${apiUrl}search?type=track&q=${term}`;
+    try{
+      return fetch(searchUrl,{
+        headers: {Authorization: `Bearer ${this.getAccessToken()}`}
+      })
+      .then(response => response.json(), rejection => {
+        throw new Error(rejection);
+      })
+      .then(jsonResponse => {
+        return jsonResponse.tracks.items.map(track => {
+          return {
+            id: track.id,
+            name: track.name,
+            artist: track.artists[0].name,
+            album: track.album.name,
+            uri: track.uri,
+            previewUrl: track.preview_url,
+            spotifyLink: track.external_urls.spotify
+          }
+        })
+      }, rejection => {
+        throw new Error(rejection);
+      });
+    }
+    catch(error){
+      console.log(error);
+      return [];
+    }
+  },
+  savePlaylist: async function(playlistName, trackUris){
+    if(playlistName==='' || trackUris.length === 0){
+      return;
+    }
+    let userAccessToken = this.getAccessToken();
+    try{
+      if(userAccessToken === undefined){
+        throw new Error("userAccessToken is undefined");
+      }
+      let headers = {Authorization: `Bearer ${userAccessToken}`};
+
+      //GET https://api.spotify.com/v1/me
+      let userId = await fetch(`${crossUrl}${apiUrl}me`,{headers: headers})
+      .then(response => response.json(), rejection => {
+        throw new Error(rejection);
+      })
+      .then(jsonResponse => jsonResponse.id, rejection => {
+        throw new Error(rejection);
+      });
+
+      if(userId === undefined){
+        throw new Error('userId is undefined');
+      }
+      headers['Content-Type'] = 'application/json';
+
+      //POST https://api.spotify.com/v1/users/{user_id}/playlists
+      let playlistId = await fetch(`${crossUrl}${apiUrl}users/${userId}/playlists`,{
+        method: 'POST',
+        body: JSON.stringify({name: playlistName}),
+        headers: headers
+      })
+      .then(response => response.json(), rejection => {
+        throw new Error(rejection);
+      })
+      .then(jsonResponse => jsonResponse.id, rejection => {
+        throw new Error(rejection);
+      });
+
+      if(playlistId === undefined){
+        throw new Error('playlistId is undefined');
+      }
+
+      //POST https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks
+      return await fetch(`${crossUrl}${apiUrl}users/${userId}/playlists/${playlistId}/tracks`,{
+        method: 'POST',
+        body: JSON.stringify({"uris": trackUris}),
+        headers: headers
+      }).then(response => {
+        return response.ok;
+      }, rejection => {
+        throw new Error(rejection);
+      });
+    }
+    catch(error){
+      console.log(error);
+      return false;
+    }
+  }
+}
+
+export {Spotify};
